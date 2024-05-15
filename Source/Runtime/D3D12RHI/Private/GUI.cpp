b56d15fcf84bcf49874df04caf02e56d47cf2511
Savion Wan <364321644@qq.com>
2024-05-15 17:49:31 +0800

    update library

--- Source/Runtime/D3D12RHI/Private/GUI.cpp
+++ Source/Runtime/D3D12RHI/Private/GUI.cpp
@@ -10,6 +10,9 @@
 #include <ShaderResourceView.h>
 #include <Texture.h>
 #include <imgui.h>
+#ifdef WIN32
+#include <imgui_impl_win32.h>
+#endif
 
 // Include compiled shaders for ImGui.
 //#include <imgui_impl_win32.h>
@@ -41,40 +44,11 @@ GUI::GUI( Device& device, HWND hWnd, const RenderTarget& renderTarget )
     m_pImGuiCtx = ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO();
 
-    ////////ImGui_ImplWin32_Init////////////
-    //if ( !ImGui_ImplWin32_Init( m_hWnd ) )
-    //{
-    //    throw std::exception( "Failed to initialize ImGui" );
-    //}
-
-    io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;         // We can honor GetMouseCursor() values (optional)
-    io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;          // We can honor io.WantSetMousePos requests (optional, rarely used)
-    io.BackendPlatformName = "imgui_impl_dx12";
-
-    // Keyboard mapping. ImGui will use those indices to peek into the io.KeysDown[] array that we will update during the application lifetime.
-    io.KeyMap[ImGuiKey_Tab] = VK_TAB;
-    io.KeyMap[ImGuiKey_LeftArrow] = VK_LEFT;
-    io.KeyMap[ImGuiKey_RightArrow] = VK_RIGHT;
-    io.KeyMap[ImGuiKey_UpArrow] = VK_UP;
-    io.KeyMap[ImGuiKey_DownArrow] = VK_DOWN;
-    io.KeyMap[ImGuiKey_PageUp] = VK_PRIOR;
-    io.KeyMap[ImGuiKey_PageDown] = VK_NEXT;
-    io.KeyMap[ImGuiKey_Home] = VK_HOME;
-    io.KeyMap[ImGuiKey_End] = VK_END;
-    io.KeyMap[ImGuiKey_Insert] = VK_INSERT;
-    io.KeyMap[ImGuiKey_Delete] = VK_DELETE;
-    io.KeyMap[ImGuiKey_Backspace] = VK_BACK;
-    io.KeyMap[ImGuiKey_Space] = VK_SPACE;
-    io.KeyMap[ImGuiKey_Enter] = VK_RETURN;
-    io.KeyMap[ImGuiKey_Escape] = VK_ESCAPE;
-    io.KeyMap[ImGuiKey_KeyPadEnter] = VK_RETURN;
-    io.KeyMap[ImGuiKey_A] = 'A';
-    io.KeyMap[ImGuiKey_C] = 'C';
-    io.KeyMap[ImGuiKey_V] = 'V';
-    io.KeyMap[ImGuiKey_X] = 'X';
-    io.KeyMap[ImGuiKey_Y] = 'Y';
-    io.KeyMap[ImGuiKey_Z] = 'Z';
-    ////////ImGui_ImplWin32_Init////////////
+#ifdef WIN32
+    if (!ImGui_ImplWin32_Init(m_hWnd)) {
+        return;
+    }
+#endif
 
     io.ConfigWindowsMoveFromTitleBarOnly = true;
 
